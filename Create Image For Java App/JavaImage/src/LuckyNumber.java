e7df003158013011dc6236f3d7a16161d18290b7
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-11-20 01:24:10 +0300

    Program Setup

---
+++ Create Image For Java App/JavaImage/src/LuckyNumber.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class LuckyNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        // Generate a random number between 1 and 10
+        int luckyNumber = random.nextInt(10) + 1;
+
+        System.out.println("Welcome to the Lucky Number Game!");
+        System.out.println("Guess a number between 1 and 10:");
+
+        try {
+            // Read user input
+            int userGuess = scanner.nextInt();
+
+            // Check if the guess is correct
+            if (userGuess == luckyNumber) {
+                System.out.println("Congratulations! You guessed the lucky number: " + luckyNumber);
+            } else {
+                System.out.println("Sorry, the lucky number was: " + luckyNumber);
+            }
+        } catch (Exception e) {
+            // Handle invalid input
+            System.out.println("Invalid input! Please enter a valid number between 1 and 10.");
+        } finally {
+            // Close the scanner
+            scanner.close();
+            System.out.println("Thanks for playing the Lucky Number Game!");
+        }
+    }
+}
